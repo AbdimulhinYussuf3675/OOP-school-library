2a3815b1a941a86ccd917a32c6a5d187a6c72ef2
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-13 11:56:47 +0300

    Resolve ArgumentError

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :age, :name
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
